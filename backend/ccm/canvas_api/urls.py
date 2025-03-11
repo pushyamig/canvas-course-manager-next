0a48266542574e588dc0331b6aa1c9f2e867d6db
Pushyami Gundala <pushyami@umich.edu>
2025-03-11 13:02:26 -0400

    #511 section creating first integration

--- backend/ccm/canvas_api/urls.py
+++ backend/ccm/canvas_api/urls.py
@@ -1,6 +1,7 @@
-from backend.ccm.canvas_api.course_api_handler import CanvasCourseAPIHandler
+from backend.ccm.canvas_api.course_api_handler import CanvasCourseAPIHandler, CanvasCourseSectionsAPIHandler
 from django.urls import path
 
 urlpatterns = [
-  path('course/<int:course_id>/', CanvasCourseAPIHandler.as_view() , name='course'),
+  path('course/<int:course_id>/', CanvasCourseAPIHandler.as_view(), name='course'),
+  path('course/<int:course_id>/sections/', CanvasCourseSectionsAPIHandler.as_view(), name='course_sections'),
 ]
