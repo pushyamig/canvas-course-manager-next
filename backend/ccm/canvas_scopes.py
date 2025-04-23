b494dbf99390520d2e26d9ef1505b7807a3820f8
Pushyami Gundala <pushyamig@users.noreply.github.com>
2025-04-23 13:07:33 -0400

    Issue #476, #510, #511 create section (#517)
    * #476 Creating bulk sections and Error Handler Refactor

--- backend/ccm/canvas_scopes.py
+++ backend/ccm/canvas_scopes.py
@@ -4,4 +4,5 @@ DEFAUlT_CANVAS_SCOPES = [
     'url:PUT|/api/v1/courses/:id',
     # Sections
     'url:GET|/api/v1/courses/:course_id/sections',
+    'url:POST|/api/v1/courses/:course_id/sections'
 ]
